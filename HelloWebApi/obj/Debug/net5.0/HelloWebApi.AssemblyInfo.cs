6d295fc4d5c0d7cbdf776bb1a3f3efcfd20b3605
Berk Akın <berkakin99@hotmail.com>
2024-09-04 20:33:21 +0300

    cache update

--- HelloWebApi/obj/Debug/net5.0/HelloWebApi.AssemblyInfo.cs
+++ HelloWebApi/obj/Debug/net5.0/HelloWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69d0624d6b2fb4451f3864333faedc5954c2989")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
